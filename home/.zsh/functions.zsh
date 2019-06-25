12c474de34e4b169145dcf1030c4682b768fcf05
Akshay Bazad <AkshayBz@yahoo.co.in>
2019-06-25 21:29:53 +0530

    httpp function

--- home/.zsh/functions.zsh
+++ home/.zsh/functions.zsh
@@ -1,9 +1,14 @@
-zsh-reload() {
+function zsh-reload {
     # exec zsh
     source ~/.zshrc
 }
 
 # Helper function to check if a program exists
-exists() {
+function exists {
     command -v "$1" >/dev/null 2>&1
+}
+
+# Use less as pager for httpie
+function httpp {
+    http --pretty=all --print=hb "$@" | less -R;
 }
