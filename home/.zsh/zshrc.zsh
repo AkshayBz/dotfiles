4c823a034d40fa75fbbc3449d57d13fa444f9e3f
Akshay Bazad <AkshayBz@yahoo.co.in>
2019-06-27 01:53:48 +0530

    Variable for profiling

--- home/.zsh/zshrc.zsh
+++ home/.zsh/zshrc.zsh
@@ -10,15 +10,19 @@
 export ZPLUG_HOME=${HOME}/.zsh/zplug
 export NVM_LAZY_LOAD=true
 export PHPBREW_LAZY_LOAD=true
+ENABLE_PROFILING=
 
-zmodload zsh/zprof
+[ $ENABLE_PROFILING ] && zmodload zsh/zprof
 source ${HOME}/.zsh/zplug.zsh
 
 # Get rid of conflicting aliases
 unalias d 2> /dev/null
 
 if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
-    source /etc/profile.d/vte.sh
+  source /etc/profile.d/vte.sh
 fi
 
-zprof >! ~/zprof.log
+if [ $ENABLE_PROFILING ]; then
+  echo 'Profiling...'
+  zprof >! ~/zprof.log
+fi
